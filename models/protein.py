68f815624f58a87b00a44acb1679fb5ca28a80f1
htyao <anthonyhtyao@gmail.com>
2017-10-30 17:56:57 +0100

    Modify atom's printer format

--- models/protein.py
+++ models/protein.py
@@ -1,5 +1,16 @@
 import dockerasmus.pdb as pdb
 
+def iter_chain_atoms(self):
+    """
+    Yield every atom and its chain's id in ``self``. Similar to self.iteratoms()
+    """
+    for chain in self.itervalues():
+        for residue in chain.itervalues():
+            for atom in sorted(residue.itervalues(), key=lambda a: a.id):
+                yield atom, chain.id
+
+pdb.Protein.iter_chain_atoms = iter_chain_atoms
+
 def write_atoms(self,path):  
     """
     A naive protein's atoms' records printer.
@@ -7,13 +18,13 @@ def write_atoms(self,path):
     Keyword arguments:
     path -- the output file's path
     """
-    # TODO : atom format uncompleted. Atom's residue should be writen. 
     atom_template = '{:7}{:4d}  {:4}{:3} {:1} {:3d}      {:5.3f}  {:5.3f}  {:5.3f}'
     out = open(path,'w')
-    for atom in self.iteratoms():
-        print(atom_template.format("ATOM",atom.id,atom.name,"ARG","B",123,atom.x,atom.y,atom.z), file=out)
+    for atom, c in self.iter_chain_atoms():
+        print(atom_template.format("ATOM",atom.id,atom.name,atom.residue.name,c,atom.residue.id,atom.x,atom.y,atom.z), file=out)
     print("END", file=out)
     out.close()
+
 
 def trans_rotate(self,trans=None,rotate=None):
     """
