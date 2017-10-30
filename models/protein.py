3e7fd5c3bb4cc90faca6e1f8e522af95582fc46d
htyao <anthonyhtyao@gmail.com>
2017-10-30 16:55:35 +0100

    A simple pipeline

--- models/protein.py
+++ models/protein.py
@@ -1,12 +1,26 @@
 import dockerasmus.pdb as pdb
 
 def write_atoms(self,path):  
+    """
+    A naive protein's atoms' records printer.
+
+    Keyword arguments:
+    path -- the output file's path
+    """
+    # TODO : atom format uncompleted. Atom's residue should be writen. 
     atom_template = '{:7}{:4d}  {:4}{:3} {:1} {:3d}      {:5.3f}  {:5.3f}  {:5.3f}'
     out = open(path,'w')
     for atom in self.iteratoms():
-        print(atom_template.format("ATOM",atom.id,atom.name,"ARG","A",123,atom.x,atom.y,atom.z), file=out)
+        print(atom_template.format("ATOM",atom.id,atom.name,"ARG","B",123,atom.x,atom.y,atom.z), file=out)
     print("END", file=out)
     out.close()
 
-pdb.Protein.write_atoms = write_atoms
+def trans_rotate(self,trans=None,rotate=None):
+    """
+    The function takes translateion and rotation's parameters, retruns atoms' positions translated and rotated matrix.
+    """
+    pass
 
+
+pdb.Protein.write_atoms = write_atoms
+pdb.Protein.trans_rotate = trans_rotate
