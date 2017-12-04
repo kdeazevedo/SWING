641f45c87e7c99b14eeb1cd3d51f40217c784d58
htyao <anthonyhtyao@gmail.com>
2017-12-04 02:54:15 +0100

    Add distibution option in arguments parser

--- argParser.py
+++ argParser.py
@@ -6,6 +6,8 @@ parent_parser.add_argument('-o', default='out',
         help='The directory where program\'s output sotres')
 parent_parser.add_argument('-rec', required=True, default=argparse.SUPPRESS,
         help='Recepter\'s file path')
+parent_parser.add_argument('-lig', required=True, default=argparse.SUPPRESS,
+        help='Ligand\'s file path')
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest='cmd')
@@ -14,9 +16,9 @@ subparsers = parser.add_subparsers(dest='cmd')
 parser_run = subparsers.add_parser('run',
     parents=[parent_parser],formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-parser_run.add_argument('-lig', required=True, default=argparse.SUPPRESS,
-        help='Ligand\'s file path')
 parser_run.add_argument('-n',type=int,default=1000,help='Number of sampling')
+parser_run.add_argument('-d',default='uniform', choices = ["uniform","normal"],
+        help='Sampling distribution')
 parser_run.add_argument('--minimizer', action='store_true',
         help='Launch minimizer after sampling')
 
@@ -24,27 +26,23 @@ parser_run.add_argument('--minimizer', action='store_true',
 parser_samples = subparsers.add_parser('samples',
         parents=[parent_parser],formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         conflict_handler='resolve')
-parser_samples.add_argument('-lig', required=True, default=argparse.SUPPRESS,
-        help='Ligand\'s file path')
 parser_samples.add_argument('-n',type=int,default=1000,help='Number of sampling')
 parser_samples.add_argument('-c','--config', required=True, default=argparse.SUPPRESS,
         help='Config file for sampling')
+parser_samples.add_argument('-d',default='uniform', choices = ["uniform","normal"],
+        help='Sampling distribution')
 parser_samples.add_argument('--minimizer', action='store_true',
         help='Launch minimizer after sampling')
 
 # Parser for alignement
 parser_align = subparsers.add_parser('align',
         parents=[parent_parser],formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_align.add_argument('-lig', required=True, default=argparse.SUPPRESS,
-        help='Ligand\'s file path')
 parser_align.add_argument('-c','--config', required=True, default=argparse.SUPPRESS,
         help='Config file for alignement')
 
 # Parser for download from InterEvol
 parser_dl = subparsers.add_parser('download',
         parents=[parent_parser],formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_dl.add_argument('-lig', required=True, default=argparse.SUPPRESS,
-        help='Ligand\'s file path')
 
 if __name__ == "__main__":
     p = parser.parse_args(['-rec','t','samples','-lig','u'])
